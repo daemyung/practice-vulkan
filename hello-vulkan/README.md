1e98db726e7c9c1b3895e8f8e4d2d78962efc31b
Daemyung Jang <dm86.jang@gmail.com>
2024-02-19 10:04:50 +0900

    Implement Hello Vulkan

---
+++ hello-vulkan/README.md
@@ -0,0 +1,24 @@
+![header](https://capsule-render.vercel.app/api?type=waving&color=gradient&height=256&text=삼각형의%20실전!%20Vulkan)
+
+# 반갑습니다! 🤗
+
+[![Vulkan](https://img.shields.io/badge/Vulkan-ac162c?style=for-the-badge&logo=vulkan)](https://https://www.vulkan.org)
+[![Inflearn](https://img.shields.io/badge/-Inflearn-brightgreen?style=for-the-badge)]()
+[![Discord](https://img.shields.io/badge/Discord-%235865F2.svg?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/vRw6xDUPKA)
+[![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/djang-88b01b91)
+
+------------------------------------------------------------------------------------------------------------------------
+
+## 📖 개요
+
+이 저장소의 코드는 [삼각형의 실전! Vulkan 중급]() 강의를 위해서 생성되었습니다. \
+강의에서 코드에 대한 설명을 들을 수 있기 때문에 코드를 위한 설명은 별도로 제공하지 않습니다. \
+학생이신 경우에 저에게 이메일을 보내주시면 **50%** 할인을 지원해 드리겠습니다.
+
+## 🎓 학습 방법
+예제를 실행하고 수정하면서 Vulkan과 친해질 수 있습니다. \
+이해가 안되거나 궁금한게 생기면 바로 질문하면 학습 효율을 높힐 수 있습니다.
+
+## 🙋 질문 방법
+
+질문은 인프런을 통하거나 이슈를 생성해서 물어보면 됩니다.
---
+++ hello-vulkan/app/src/main/cpp/main.cpp
@@ -0,0 +1,72 @@
+#include <game-activity/GameActivity.cpp>
+#include <game-text-input/gametextinput.cpp>
+#include <vulkan/vulkan.h>
+
+#include "AndroidOut.h"
+
+extern "C" {
+
+#include <game-activity/native_app_glue/android_native_app_glue.c>
+
+/*!
+ * Handles commands sent to this Android application
+ * @param pApp the app the commands are coming from
+ * @param cmd the command to handle
+ */
+void handle_cmd(android_app *pApp, int32_t cmd) {
+    switch (cmd) {
+        case APP_CMD_INIT_WINDOW:
+            aout << "Hello Vulkan!" << std::endl;
+            break;
+        case APP_CMD_TERM_WINDOW:
+            break;
+        default:
+            break;
+    }
+}
+
+/*!
+ * Enable the motion events you want to handle; not handled events are
+ * passed back to OS for further processing. For this example case,
+ * only pointer and joystick devices are enabled.
+ *
+ * @param motionEvent the newly arrived GameActivityMotionEvent.
+ * @return true if the event is from a pointer or joystick device,
+ *         false for all other input devices.
+ */
+bool motion_event_filter_func(const GameActivityMotionEvent *motionEvent) {
+    auto sourceClass = motionEvent->source & AINPUT_SOURCE_CLASS_MASK;
+    return (sourceClass == AINPUT_SOURCE_CLASS_POINTER ||
+            sourceClass == AINPUT_SOURCE_CLASS_JOYSTICK);
+}
+
+/*!
+ * This the main entry point for a native activity
+ */
+void android_main(struct android_app *pApp) {
+    // Register an event handler for Android events
+    pApp->onAppCmd = handle_cmd;
+
+    // Set input event filters (set it to NULL if the app wants to process all inputs).
+    // Note that for key inputs, this example uses the default default_key_filter()
+    // implemented in android_native_app_glue.c.
+    android_app_set_motion_event_filter(pApp, motion_event_filter_func);
+
+    // This sets up a typical game/event loop. It will run until the app is destroyed.
+    int events;
+    android_poll_source *pSource;
+    do {
+        // Process all pending events before running game logic.
+        if (ALooper_pollAll(0, nullptr, &events, (void **) &pSource) >= 0) {
+            if (pSource) {
+                pSource->process(pApp, pSource);
+            }
+        }
+
+        // Check if any user data is associated. This is assigned in handle_cmd
+        if (pApp->userData) {
+        }
+    } while (!pApp->destroyRequested);
+}
+
+}
