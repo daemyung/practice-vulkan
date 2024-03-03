7d68c8f637a57033abb1e3244e0f8410aa4d82cf
Daemyung Jang <dm86.jang@gmail.com>
2024-03-04 00:09:50 +0900

    Implement Vulkan Display extensions

---
+++ vulkan-display-extensions/app/src/main/cpp/VkRenderer.cpp
@@ -0,0 +1,179 @@
+// MIT License
+//
+// Copyright (c) 2024 Daemyung Jang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <cassert>
+#include <array>
+#include <vector>
+#include <iomanip>
+
+#include "VkRenderer.h"
+#include "VkUtil.h"
+#include "AndroidOut.h"
+
+using namespace std;
+
+VkRenderer::VkRenderer() {
+    // ================================================================================
+    // 1. VkInstance 생성
+    // ================================================================================
+    VkApplicationInfo applicationInfo{
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Practice Vulkan",
+        .applicationVersion = VK_MAKE_API_VERSION(0, 0, 1, 0),
+        .apiVersion = VK_MAKE_API_VERSION(0, 1, 3, 0)
+    };
+
+    uint32_t instanceLayerCount;
+    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&instanceLayerCount, nullptr));
+
+    vector<VkLayerProperties> instanceLayerProperties(instanceLayerCount);
+    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&instanceLayerCount,
+                                                      instanceLayerProperties.data()));
+
+    vector<const char *> instanceLayerNames;
+    for (const auto &properties: instanceLayerProperties) {
+        instanceLayerNames.push_back(properties.layerName);
+    }
+
+    uint32_t instanceExtensionCount;
+    VK_CHECK_ERROR(vkEnumerateInstanceExtensionProperties(nullptr,
+                                                          &instanceExtensionCount,
+                                                          nullptr));
+
+    vector<VkExtensionProperties> instanceExtensionProperties(instanceExtensionCount);
+    VK_CHECK_ERROR(vkEnumerateInstanceExtensionProperties(nullptr,
+                                                          &instanceExtensionCount,
+                                                          instanceExtensionProperties.data()));
+
+    vector<const char *> instanceExtensionNames;
+    for (const auto &properties: instanceExtensionProperties) {
+        if (properties.extensionName == string("VK_KHR_surface") ||
+            properties.extensionName == string("VK_KHR_android_surface")) {
+            instanceExtensionNames.push_back(properties.extensionName);
+        }
+    }
+    assert(instanceExtensionNames.size() == 2);
+
+    VkInstanceCreateInfo instanceCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &applicationInfo,
+        .enabledLayerCount = static_cast<uint32_t>(instanceLayerNames.size()),
+        .ppEnabledLayerNames = instanceLayerNames.data(),
+        .enabledExtensionCount = static_cast<uint32_t>(instanceExtensionNames.size()),
+        .ppEnabledExtensionNames = instanceExtensionNames.data()
+    };
+
+    VK_CHECK_ERROR(vkCreateInstance(&instanceCreateInfo, nullptr, &mInstance));
+
+    // ================================================================================
+    // 2. VkPhysicalDevice 선택
+    // ================================================================================
+    uint32_t physicalDeviceCount;
+    VK_CHECK_ERROR(vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, nullptr));
+
+    vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+    VK_CHECK_ERROR(vkEnumeratePhysicalDevices(mInstance,
+                                              &physicalDeviceCount,
+                                              physicalDevices.data()));
+
+    mPhysicalDevice = physicalDevices[0];
+
+    VkPhysicalDeviceProperties physicalDeviceProperties;
+    vkGetPhysicalDeviceProperties(mPhysicalDevice, &physicalDeviceProperties);
+
+    aout << "Selected Physical Device Information ↓" << endl;
+    aout << setw(16) << left << " - Device Name: "
+         << string_view(physicalDeviceProperties.deviceName) << endl;
+    aout << setw(16) << left << " - Device Type: "
+         << vkToString(physicalDeviceProperties.deviceType) << endl;
+    aout << std::hex;
+    aout << setw(16) << left << " - Device ID: " << physicalDeviceProperties.deviceID << endl;
+    aout << setw(16) << left << " - Vendor ID: " << physicalDeviceProperties.vendorID << endl;
+    aout << std::dec;
+    aout << setw(16) << left << " - API Version: "
+         << VK_API_VERSION_MAJOR(physicalDeviceProperties.apiVersion) << "."
+         << VK_API_VERSION_MINOR(physicalDeviceProperties.apiVersion);
+    aout << setw(16) << left << " - Driver Version: "
+         << VK_API_VERSION_MAJOR(physicalDeviceProperties.driverVersion) << "."
+         << VK_API_VERSION_MINOR(physicalDeviceProperties.driverVersion);
+
+    // ================================================================================
+    // 3. VkDevice 생성
+    // ================================================================================
+    uint32_t queueFamilyPropertiesCount;
+    vkGetPhysicalDeviceQueueFamilyProperties(mPhysicalDevice, &queueFamilyPropertiesCount, nullptr);
+
+    vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyPropertiesCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(mPhysicalDevice, &queueFamilyPropertiesCount,
+                                             queueFamilyProperties.data());
+
+    for (mQueueFamilyIndex = 0;
+         mQueueFamilyIndex != queueFamilyPropertiesCount; ++mQueueFamilyIndex) {
+        if (queueFamilyProperties[mQueueFamilyIndex].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            break;
+        }
+    }
+
+    const vector<float> queuePriorities{1.0};
+    VkDeviceQueueCreateInfo deviceQueueCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueFamilyIndex = mQueueFamilyIndex,
+        .queueCount = 1,
+        .pQueuePriorities = queuePriorities.data()
+    };
+
+    uint32_t deviceExtensionCount;
+    VK_CHECK_ERROR(vkEnumerateDeviceExtensionProperties(mPhysicalDevice,
+                                                        nullptr,
+                                                        &deviceExtensionCount,
+                                                        nullptr));
+
+    vector<VkExtensionProperties> deviceExtensionProperties(deviceExtensionCount);
+    VK_CHECK_ERROR(vkEnumerateDeviceExtensionProperties(mPhysicalDevice,
+                                                        nullptr,
+                                                        &deviceExtensionCount,
+                                                        deviceExtensionProperties.data()));
+
+    vector<const char *> deviceExtensionNames;
+    for (const auto &properties: deviceExtensionProperties) {
+        if (properties.extensionName == string("VK_KHR_swapchain")) {
+            deviceExtensionNames.push_back(properties.extensionName);
+        }
+    }
+    assert(deviceExtensionNames.size() == 1);
+
+    VkDeviceCreateInfo deviceCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .queueCreateInfoCount = 1,
+        .pQueueCreateInfos = &deviceQueueCreateInfo,
+        .enabledExtensionCount = static_cast<uint32_t>(deviceExtensionNames.size()),
+        .ppEnabledExtensionNames = deviceExtensionNames.data()
+    };
+
+    VK_CHECK_ERROR(vkCreateDevice(mPhysicalDevice, &deviceCreateInfo, nullptr, &mDevice));
+    vkGetDeviceQueue(mDevice, mQueueFamilyIndex, 0, &mQueue);
+}
+
+VkRenderer::~VkRenderer() {
+    vkDestroyDevice(mDevice, nullptr);
+    vkDestroyInstance(mInstance, nullptr);
+}
