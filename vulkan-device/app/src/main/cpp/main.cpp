cad566a5992b08ba1530789495e5e832b6cc1007
Daemyung Jang <dm86.jang@gmail.com>
2024-03-03 11:16:52 +0900

    Implement Vulkan Device

---
+++ vulkan-device/app/src/main/cpp/main.cpp
@@ -0,0 +1,75 @@
+#include <game-activity/GameActivity.cpp>
+#include <game-text-input/gametextinput.cpp>
+
+#include "VkRenderer.h"
+#include "AndroidOut.h"
+
+extern "C" {
+
+#include <game-activity/native_app_glue/android_native_app_glue.c>
+
+/*!
+ * Handles commands sent to this Android application
+ * @param pApp the app the commands are coming from
+ * @param cmd the command to handle
+ */
+void handle_cmd(android_app *pApp, int32_t cmd) {
+    switch (cmd) {
+        case APP_CMD_INIT_WINDOW:
+            pApp->userData = new VkRenderer();
+            break;
+        case APP_CMD_TERM_WINDOW:
+            if (pApp->userData) {
+                delete static_cast<VkRenderer *>(pApp->userData);
+                pApp->userData = nullptr;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+/*!
+ * Enable the motion events you want to handle; not handled events are
+ * passed back to OS for further processing. For this example case,
+ * only pointer and joystick devices are enabled.
+ *
+ * @param motionEvent the newly arrived GameActivityMotionEvent.
+ * @return true if the event is from a pointer or joystick device,
+ *         false for all other input devices.
+ */
+bool motion_event_filter_func(const GameActivityMotionEvent *motionEvent) {
+    auto sourceClass = motionEvent->source & AINPUT_SOURCE_CLASS_MASK;
+    return (sourceClass == AINPUT_SOURCE_CLASS_POINTER ||
+            sourceClass == AINPUT_SOURCE_CLASS_JOYSTICK);
+}
+
+/*!
+ * This the main entry point for a native activity
+ */
+void android_main(struct android_app *pApp) {
+    // Register an event handler for Android events
+    pApp->onAppCmd = handle_cmd;
+
+    // Set input event filters (set it to NULL if the app wants to process all inputs).
+    // Note that for key inputs, this example uses the default default_key_filter()
+    // implemented in android_native_app_glue.c.
+    android_app_set_motion_event_filter(pApp, motion_event_filter_func);
+
+    // This sets up a typical game/event loop. It will run until the app is destroyed.
+    int events;
+    android_poll_source *pSource;
+    do {
+        // Process all pending events before running game logic.
+        if (ALooper_pollAll(0, nullptr, &events, (void **) &pSource) >= 0) {
+            if (pSource) {
+                pSource->process(pApp, pSource);
+            }
+        }
+
+        // Check if any user data is associated. This is assigned in handle_cmd
+        if (pApp->userData) {
+        }
+    } while (!pApp->destroyRequested);
+}
+}
